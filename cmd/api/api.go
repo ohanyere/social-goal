e08916bfbec069fb53c21d265632a7414948c724
Ohanyere Francis <mooref068@gmail.com>
2026-01-18 08:42:02 -0800

    feat: implemented routing using chi

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -3,6 +3,9 @@ package main
 import (
 	"net/http"
 	"time"
+	"github.com/go-chi/chi/v5"
+  	"github.com/go-chi/chi/v5/middleware"
+
 )
 
 type application struct {
@@ -13,13 +16,30 @@ type config struct {
 	addrs string
 }
 
-func (app application) run() error {
-	mux := http.NewServeMux()
+func (app *application) mount() *chi.Mux{
+	  r := chi.NewRouter()
+  // A good base middleware stack
+		r.Use(middleware.RequestID)
+		r.Use(middleware.RealIP)
+		r.Use(middleware.Logger)
+		r.Use(middleware.Recoverer)
+	
+    r.Use(middleware.Timeout(60 * time.Second))
+	r.Route("/v1", func(r chi.Router) {
+		r.Get("/health", app.HealthCheck)
+	})
+  	
+	return  r
+}
+
+func (app *application) run() error {
+	mux := app.mount()
 	server := &http.Server{
 		Addr:  app.config.addrs,
 		Handler : mux,
 		WriteTimeout: time.Second * 30,
 		ReadTimeout: time.Second * 10,
+		IdleTimeout: time.Minute,
 	}
 
 	return  server.ListenAndServe()

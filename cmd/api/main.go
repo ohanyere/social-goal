3885b8726fc47ba41f227af0b5eab3d4bd4c4ee8
Ohanyere Francis <mooref068@gmail.com>
2026-01-20 04:01:56 -0800

    feat: add env

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,20 @@
 package main
 
-import "log"
+import (
+	"log"
+	"os"
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("server_port")
 	cfg := config{
-		addrs: ":8080",
+		addrs: port,
 	}
 
 	app := application{
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module social-post
 
 go 1.25.5
 
-require github.com/go-chi/chi/v5 v5.2.4 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.2.4 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
